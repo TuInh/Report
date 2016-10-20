c0f8ede829ec669a4ed2cc0a2c305069c45796be
Le Thi Van Anh <le.thi.van.anh@macmini0017s-mac-mini.local>
2016-10-20 16:01:25 +0700

    Initial Commit

---
+++ InternationalizationDe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InternationalizationDe
+//
+//  Created by Le Thi Van Anh on 10/20/16.
+//  Copyright © 2016 Le Thi Van Anh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
