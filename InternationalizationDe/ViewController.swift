9c59112fd5ddc60dca5cfad0d578144da29188dd
Le Thi Van Anh <le.thi.van.anh@macmini0017s-Mac-mini.local>
2016-10-25 13:33:20 +0700

    Report T10

--- InternationalizationDe/ViewController.swift
+++ InternationalizationDe/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var vietnameseBtn: UIButton!
+    @IBOutlet weak var englishBtn: UIButton!
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var passTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var passLabel: UILabel!   
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var bundle: NSBundle = NSBundle(){
+        didSet{
+            localizeString()
+        }
+    }
+    
+    func localizeString(){
+        nameTextField.placeholder =  NSLocalizedString("kUserName", bundle: bundle, comment: "hello")
+        passTextField.placeholder = NSLocalizedString("kPassword", bundle: bundle, comment: "hello")
+        nameLabel.text =  NSLocalizedString("kName", bundle: bundle, comment: "hello")
+        passLabel.text =  NSLocalizedString("kPwd", bundle: bundle, comment: "hello")
+        loginBtn.setTitle(NSLocalizedString("kLogin", bundle: bundle, comment: "hello"), forState: UIControlState.Normal)
+        englishBtn.setTitle(NSLocalizedString("kEnglish", bundle: bundle, comment: "hello"), forState: UIControlState.Normal)
+        vietnameseBtn.setTitle(NSLocalizedString("kVietNam", bundle: bundle, comment: "hello"), forState: UIControlState.Normal)
+        
+    }
+
+    @IBAction func chooseEnglish(sender: AnyObject) {
+        LanguageManager.sharedInstance.setLocale("en")
+        bundle = LanguageManager.sharedInstance.getCurrentBundle()
+    }
+    
+    @IBAction func chooseVietNam(sender: AnyObject) {
+        LanguageManager.sharedInstance.setLocale("vi")
+        bundle = LanguageManager.sharedInstance.getCurrentBundle()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        bundle = LanguageManager.sharedInstance.getCurrentBundle()
+        
     }
 
     override func didReceiveMemoryWarning() {
